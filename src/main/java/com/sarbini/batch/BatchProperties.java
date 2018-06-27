fa3d11d79fb0b8bb0e8b62d9843fa6b0193d20ed
mhamed-blagui <mhamed.blagui88@gmail.com>
2018-06-27 22:25:18 +0100

    master

---
+++ src/main/java/com/sarbini/batch/BatchProperties.java
@@ -0,0 +1,13 @@
+package com.sarbini.batch;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+@ConfigurationProperties
+public class BatchProperties {
+
+}
